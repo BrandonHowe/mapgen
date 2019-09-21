c121feaf2fe6d2db550f18a82d9c95c76bec4364
xWafl <73sampleperson@gmail.com>
2019-09-21 16:57:05 -0400

    Added a shit ton of things

--- display.js
+++ display.js
@@ -1,25 +1,83 @@
 var canvas = document.getElementById("myCanvas");
+var dcanvas = document.getElementById("dotcanvas");
 canvas.width = 225;
 canvas.height = 225;
+dotcanvas.width = 225;
+dotcanvas.height = 225;
 var canvasWidth = canvas.width;
 var canvasHeight = canvas.height;
 var ctx = canvas.getContext("2d");
+var dtx = dcanvas.getContext("2d");
 
-for (let i = 0; i < planetsize; i++) {
-    for (let j = 0; j < planetsize; j++) {
-        switch (planet[i][j].terrain) {
-            case "tundra": 
-                ctx.fillStyle = "#FFF"
-                ctx.fillRect(i,j,2,2); // fill in the pixel at (10,10)
-                break;
-            case "ocean":
-                ctx.fillStyle = "#00F"
-                ctx.fillRect(i,j,2,2); // fill in the pixel at (10,10)
-                break;
-            case "land":
-                ctx.fillStyle = "#0F0"
-                ctx.fillRect(i,j,2,2); // fill in the pixel at (10,10)
-                break;
+const drawPlanet = () => {
+    for (let i = 0; i < planetsize; i++) {
+        for (let j = 0; j < planetsize; j++) {
+            switch (planet[i][j].terrain) {
+                case "tundra": 
+                    ctx.fillStyle = "#FFF"
+                    ctx.fillRect(i,j,2,2); // fill in the pixel at (10,10)
+                    break;
+                case "ocean":
+                    ctx.fillStyle = `hsl(240, 100%, 50%)`;
+                    ctx.fillRect(i,j,2,2); // fill in the pixel at (10,10)
+                    break;
+                case "land":
+                    ctx.fillStyle = `hsl(${(planet[i][j].hue)}, ${planet[i][j].altitude / 1.6}%, ${(planet[i][j].altitude / 50) + 50}%)`;
+                    ctx.fillRect(i,j,2,2); // fill in the pixel at (10,10)
+                    break;
+                case "mountain":
+                    ctx.fillStyle = `hsl(${(planet[i][j].hue)}, 25%, 70%)`;
+                    ctx.fillRect(i,j,2,2); // fill in the pixel at (10,10)
+                    break;
+                case "highhills":
+                    ctx.fillStyle = `hsl(${(planet[i][j].hue)}, 15%, 80%)`;
+                    ctx.fillRect(i,j,2,2); // fill in the pixel at (10,10)
+                    break;
+            }
         }
     }
 }
+
+let currentPosY = 0;
+let currentPosX = 0;
+
+const updateName = () => {
+    dtx.fillStyle = "#F00";
+    dtx.fillRect(currentPosX, currentPosY, 1, 1);
+    // dtx.clearRect();
+    let currentName = planet[currentPosX][currentPosY].name;
+    document.getElementById("name").innerHTML = currentName;
+}
+
+document.onkeydown = function (event) {
+    switch (event.keyCode) {
+        case 38:
+            if (planet[currentPosY - 1]) {
+                dtx.clearRect(currentPosX, currentPosY, 1, 1);
+                currentPosY--;
+            }
+            break;
+        case 39:
+            if (planet[currentPosX + 1]) {
+                dtx.clearRect(currentPosX, currentPosY, 1, 1);
+                currentPosX++;
+            }
+            break;
+        case 40:
+            if (planet[currentPosY + 1]) {
+                dtx.clearRect(currentPosX, currentPosY, 1, 1);
+                currentPosY++;
+            }
+            break;
+        case 37:
+            if (planet[currentPosX - 1]) {
+                dtx.clearRect(currentPosX, currentPosY, 1, 1);
+                currentPosX--;
+            }
+            break;
+    }
+    // console.log(currentPosX + "|" + currentPosY)
+    updateName();
+};
+
+drawPlanet();
--- index.js
+++ index.js
@@ -17,11 +17,24 @@ let planetsize = 225;
 let planet = [];
 let arcticlength = 30;
 
+const contNames = [
+    "The Land of ", "The Continent of ", "The Lands of ", "The Land of ", "The Continent of ", "The Lands of ", "The Land of "
+]
+
+const bareContNames = [
+    "Istlus", "Afangëm", "Elluem", "Leïeluth", "Aragok", "Willowile", "Ghëlaräk"
+]
+
+const mountainNames = [
+    "Duarfenheil", "Illaeïgich", "Deliyä", "Makengheil", "Malehiëen", "Swevük", "Erud Gorgoröth", "Ephël Lithui", "Taniquetil", "Erebor", "Orodruin", "Celebdhil", "Gilgakelsh", "Eril Minad", "Manis Moria", "Banderkräll", "Il-a ür","Meliämür", "Rivelthrien", "Ythërn", "Qfaäth"
+]
+
 const createPlanetArr = () => {
     for (let i = 0; i < planetsize; i++) {
         planet.push([]);
         for (let j = 0; j < planetsize; j++) {
             planet[i].push({"terrain": "ocean"});
+            planet[i][j].name = "The mysterious waters of Imperia";
         }
     }
 }
@@ -89,41 +102,290 @@ const checkIsland = (i, j) => {
     }
 }
 
-const createContinents = () => {
-    let contSize = Math.floor(Math.random() * 40) + 10;
-    let contCenterX = Math.floor(Math.random() * 200) + 10;
-    let contCenterY = Math.floor(Math.random() * 145) + 40;
-    for (let i = 0; i < planetsize; i++) {
-        for (let j = 0; j < planetsize; j++) {
-            if (Math.sqrt(Math.pow(Math.abs(contCenterX - j), 2) + Math.pow(Math.abs(contCenterY - i), 2)) <= contSize) {
+const checkLake = (i, j) => {
+    let baycheck = 0;
+    if (i === 0 || i === 224 || j === 0 || j === 224) {
+        return false;
+    }
+    if (planet[i][j - 1].terrain === "land") {
+        baycheck++;
+    }
+    if (planet[i][j + 1].terrain === "land") {
+        baycheck++;
+    }
+    if (planet[i + 1][j].terrain === "land") {
+        baycheck++;
+    }
+    if (planet[i - 1][j].terrain === "land") {
+        baycheck++;
+    }
+    if (baycheck === 4) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// const createContinents = () => {
+//     let contSize = Math.floor(Math.random() * 40) + 10;
+//     let contCenterX = Math.floor(Math.random() * 200) + 10;
+//     let contCenterY = Math.floor(Math.random() * 145) + 40;
+//     for (let i = 0; i < planetsize; i++) {
+//         for (let j = 0; j < planetsize; j++) {
+//             if (Math.sqrt(Math.pow(Math.abs(contCenterX - j), 2) + Math.pow(Math.abs(contCenterY - i), 2)) <= contSize) {
+//                 planet[i][j].terrain = "land";
+//             } else {
+//                 let taxidist = Math.abs(j - contCenterX) + Math.abs(contCenterY - i);
+//                 if (Math.random() > (taxidist - contSize) / 50) {
+//                     planet[i][j].terrain = "land";
+//                 }
+//                 if (j === 0 || j === 224) {
+//                     continue;
+//                 }
+//                 if (checkBay(i, j) === true && planet[i][j].terrain === "ocean") {
+//                     planet[i][j].terrain = "land";
+//                     console.log("bruh");
+//                 }
+//                 if (checkIsland(i, j) === true && planet[i][j].terrain === "land") {
+//                     planet[i][j].terrain = "ocean";
+//                 }
+//                 // console.log("bruh");
+//             }
+//         }
+//     }
+// }
+
+function floodfill (nodex, nodey, target, terrain) {
+    nodex = Number(nodex);
+    nodey = Number(nodey);
+    // console.log("nodex: " + nodex + "|nodey: " + nodey); 
+    if (!planet[nodey]) {
+        return;
+    } else if (!planet[nodey][nodex]) {
+        return;
+    }
+    if (planet[nodey][nodex].terrain === terrain) {
+        return 0;
+    } else if (planet[nodey][nodex].terrain !== target) {
+        return 0;
+    } else {
+        planet[nodey][nodex].terrain = terrain;
+    }
+    floodfill(Number(nodex - 1), Number(nodey), target, terrain);
+    floodfill(Number(nodex + 1), Number(nodey), target, terrain);
+    floodfill(Number(nodex), Number(nodey + 1), target, terrain);
+    floodfill(Number(nodex), Number(nodey - 1), target, terrain);
+}
+
+const joinContinents = () => {
+    for (let i = 25; i < 200; i += 25) {
+        floodfill(i, 0, "ocean", "oceandos");
+        floodfill(i, 224, "ocean", "oceandos");
+    }
+    for (let i = 25; i < 200; i++) {
+        for (let j = 0; j < 224; j++) {
+            if (planet[i][j].terrain === "ocean") {
                 planet[i][j].terrain = "land";
-            } else {
-                let taxidist = Math.abs(j - contCenterX) + Math.abs(contCenterY - i);
-                if (Math.random() > (taxidist - contSize) / 50) {
-                    planet[i][j].terrain = "land";
-                }
-                if (j === 0 || j === 224) {
-                    continue;
-                }
-                if (checkBay(i, j) === true && planet[i][j].terrain === "ocean") {
-                    planet[i][j].terrain = "land";
-                    console.log("bruh");
-                }
-                if (checkIsland(i, j) === true && planet[i][j].terrain === "land") {
-                    planet[i][j].terrain = "ocean";
-                }
-                // console.log("bruh");
+            }
+        }
+    }
+    for (let i = 25; i < 200; i += 25) {
+        floodfill(i, 0, "oceandos", "ocean");
+        floodfill(i, 224, "oceandos", "ocean");
+    }
+}
+
+const removeLakes = () => {
+    for (let i = 1; i < 223; i++) {
+        for (let j = 1; j < 223; j++) {
+            if (checkLake(i, j) === true) {
+                planet[i][j].terrain === "land";
             }
         }
     }
 }
 
+const createContinents = () => {
+    let contCenterX = Math.floor(Math.random() * 145) + 10;
+    let contCenterY = Math.floor(Math.random() * 200) + 40;
+    let currentSpotX = contCenterX;
+    let currentSpotY = contCenterY;
+    for (let i = 0; i < 10000; i++) {
+        let diceRoll = Math.floor(Math.random() * 4);
+        switch(diceRoll) {
+            case 0:
+                currentSpotX++;
+                break;
+            case 1:
+                currentSpotY++;
+                break;
+            case 2:
+                currentSpotX--;
+                break;
+            case 3:
+                currentSpotY--;
+                break;
+        }
+        // console.log(currentSpotX + "|" + currentSpotY + "|" + diceRoll);
+        if (!planet[currentSpotX]) {
+            continue;
+        }
+        if (currentSpotY === 225 || currentSpotY <= 0) {
+            continue;
+        }
+        if (currentSpotY <= arcticlength || currentSpotY >= 225 - arcticlength) {
+            planet[currentSpotX][currentSpotY].terrain = "tundra";
+        } else {
+            planet[currentSpotX][currentSpotY].terrain = "land";
+            planet[currentSpotX][currentSpotY].altitude = 100;
+            planet[currentSpotX][currentSpotY].name = contNames[contCount] + bareContNames[contCount];
+            planet[currentSpotX][currentSpotY].namenum = contCount;
+        }
+    }
+}
+
+const genMountains = (name) => {
+    let contCenterX = Math.floor(Math.random() * 200) + 10;
+    let contCenterY = Math.floor(Math.random() * 145) + 40;
+    let currentSpotX = contCenterX;
+    let currentSpotY = contCenterY;
+    while (!planet[currentSpotY][currentSpotX].terrain === "land") {
+        contCenterX = Math.floor(Math.random() * 200) + 10;
+        contCenterY = Math.floor(Math.random() * 145) + 40;
+        currentSpotX = contCenterX;
+        currentSpotY = contCenterY;
+    }
+    for (let i = 0; i < 200; i++) {
+        let diceRoll = Math.floor(Math.random() * 4);
+        switch(diceRoll) {
+            case 0:
+                currentSpotX++;
+                break;
+            case 1:
+                currentSpotY++;
+                break;
+            case 2:
+                currentSpotX--;
+                break;
+            case 3:
+                currentSpotY--;
+                break;
+        }
+        if (!planet[currentSpotX]) {
+            continue;
+        }
+        if (currentSpotY === 225 || currentSpotY <= 0) {
+            continue;
+        }
+        if (planet[currentSpotX][currentSpotY].terrain === "land") {
+            planet[currentSpotX][currentSpotY].terrain = "mountain";
+            planet[currentSpotX][currentSpotY].name = "The Mountains of " + mountainNames[name];
+        }
+    }
+}
+
+const genHighHills = () => {
+    for (let i = 1; i < planetsize - 1; i++) {
+        for (let j = 1; j < planetsize - 1; j++) {
+            if (planet[i][j].terrain === "land") {
+                if (planet[i - 1][j].terrain === "mountain" || planet[i + 1][j].terrain === "mountain" || planet[i][j + 1].terrain === "mountain" || planet[i][j - 1].terrain === "mountain") {
+                    planet[i][j].terrain = "highhills";
+                    planet[i][j].name = "The High Hills of " + bareContNames[planet[i][j].namenum];
+                }
+            }
+        }
+    }
+}
+
+const genAltitude = () => {
+    let contCenterX = Math.floor(Math.random() * 200) + 10;
+    let contCenterY = Math.floor(Math.random() * 145) + 40;
+    let currentSpotX = contCenterX;
+    let currentSpotY = contCenterY;
+    while (!planet[currentSpotY][currentSpotX].terrain === "land") {
+        contCenterX = Math.floor(Math.random() * 200) + 10;
+        contCenterY = Math.floor(Math.random() * 145) + 40;
+        currentSpotX = contCenterX;
+        currentSpotY = contCenterY;
+    }
+    for (let i = 0; i < 1000; i++) {
+        let diceRoll = Math.floor(Math.random() * 4);
+        switch(diceRoll) {
+            case 0:
+                currentSpotX++;
+                break;
+            case 1:
+                currentSpotY++;
+                break;
+            case 2:
+                currentSpotX--;
+                break;
+            case 3:
+                currentSpotY--;
+                break;
+        }
+        if (!planet[currentSpotX]) {
+            continue;
+        }
+        if (currentSpotY === 225 || currentSpotY <= 0) {
+            continue;
+        }
+        if (planet[currentSpotX][currentSpotY].terrain === "land") {
+            planet[currentSpotX][currentSpotY].altitude = 75 - Math.floor(Math.random() * 30);
+            planet[currentSpotX][currentSpotY].name = "The Hills of " + bareContNames[planet[currentSpotX][currentSpotY].namenum];
+        }
+    }
+}
+
+const genTemp = () => {
+    let equator = Math.floor(planetsize / 2);
+    for (let i = 0; i < planetsize; i++) {
+        for (let j = 0; j < planetsize; j++) {
+            if (planet[i][j].terrain === "land") {
+                let equdist = Math.abs(equator - j);
+                let randvari = Math.floor(Math.random() * 20) - 10;
+                let temp = 80 + (equdist + randvari) - avgtemp;
+                planet[i][j].temperature = temp;
+                planet[i][j].hue = temp + 60;
+            }
+        }
+    }
+}
+
+let contCount = 0;
+
+const trycatchconts = () => {
+    if (contCount >= 5) {
+        return;
+    }
+    try {
+        createContinents();
+        contCount++;
+        if (contCount < 5) {
+            trycatchconts();
+        }
+    }
+    catch {
+        trycatchconts();
+    }
+}
+
 const start = () => {
     createPlanetArr();
+    trycatchconts();
+    // joinContinents();
+    removeLakes();
     createArctic();
-    createContinents();
+    genTemp();
+    for (let i = 0; i < 20; i++) {
+        genMountains(i);
+    }
+    genHighHills();
+    for (let i = 0; i < 50; i++) {
+        genAltitude();
+    }
 }
 
 start();
 
-console.log(planet[3]);
+console.log(planet[120]);
